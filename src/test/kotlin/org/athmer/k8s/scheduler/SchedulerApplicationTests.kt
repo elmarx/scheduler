63ef1c74362ea2ea979b4679a3dc2d707aa4a6df
Elmar Athmer <elmar@athmer.org>
2024-09-30 21:48:00 +0200

    initial import

---
+++ src/test/kotlin/org/athmer/k8s/scheduler/SchedulerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.athmer.k8s.scheduler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
